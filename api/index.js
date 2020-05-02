a737870a72a3b6a96dc2ea9d632dedd8384f428c
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-05-02 17:48:47 -0400

    Add socket server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const app = express();
 
-server = app.listen(3001, function () {
-  console.log('server is running on port 3001')
+const server = app.listen(6544, function () {
+  console.log('server is running on port 6544')
+});
+
+const io = require('socket.io')(server);
+
+// let clients = [
+//   {name: "marble"},
+//   {name: "dan"}
+// ]
+
+let clients = [];
+
+io.on('connection', function (socket) {
+  console.log("connected: ", socket.id)
+  console.log(socket.client)
+  // clients.push(client);
+
+  // client.on('disconnect', function() {
+  //     clients.splice(clients.indexOf(client), 1);
+  // });
+
+  // const newClients = io.sockets.clients();
+
+  // io.emit('clients', newClients)
+
+  socket.on('send_message', function (data) {
+    io.emit('message', data)
+  });
+  // socket.on('new_client', function (data) {
+  //   io.emit('clients', data)
+  // });
 });
